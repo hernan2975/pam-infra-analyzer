0cde44ed40f2b4ec98def474e919fcf5e96c3e49
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-11-22 17:57:31 -0300

    Create Host.java

---
+++ src/main/java/ar/gob/lapampai/nfra/model/Host.java
@@ -0,0 +1,7 @@
+public class Host {
+    private String hostname;
+    private String ip;
+    private String os;
+    private String location; // ej: "Santa Rosa - Edificio Central"
+    private List<Metric> metrics;
+}
