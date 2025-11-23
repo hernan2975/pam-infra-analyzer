5222f54ed20a8837d0e70363773760c7ff12eee0
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-11-23 16:33:49 -0300

    Delete src/test directory

--- src/test/ java/ar/gob/lapampa/infra/analyzer/CpuAnalyzer.java
+++
@@ -1,7 +0,0 @@
-public class CpuAnalyzer {
-    public String assess(double load) {
-        if (load > 90) return "CRITICAL";
-        if (load > 75) return "WARNING";
-        return "OK";
-    }
-}
