8a28e371547fa453541c0c3535909f314f50314d
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-11-23 16:28:47 -0300

    Create JsonReporter.java

---
+++ src/main/java/ar/gob/lapampa/infra/reporter/JsonReporter.java
@@ -0,0 +1,11 @@
+package ar.gob.lapampa.infra.reporter;
+
+import ar.gob.lapampa.infra.model.Host;
+
+import java.io.IOException;
+
+public class JsonReporter {
+    public void writeReport(Host host) throws IOException {
+        // Placeholder — extensión futura
+    }
+}
